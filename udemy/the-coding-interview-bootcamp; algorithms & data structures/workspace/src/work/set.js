f49010e1d27820b2dbdd61002291cd03a2de4031
johnvincentio <john@johnvincent.io>
2019-06-03 13:11:16 -0400

    add

--- udemy/the-coding-interview-bootcamp; algorithms & data structures/workspace/src/work/set.js
+++ udemy/the-coding-interview-bootcamp; algorithms & data structures/workspace/src/work/set.js
@@ -35,27 +35,32 @@
 // 	console.log(key);
 // }
 
-const myMap = new Map();
-myMap.set(0, 'zero');
-myMap.set(1, 'one');
-for (const [key, value] of myMap) {
-	console.log(`${key} = ${value}`);
-}
-// 0 = zero
-// 1 = one
+// const myMap = new Map();
+// myMap.set(0, 'zero');
+// myMap.set(1, 'one');
+// for (const [key, value] of myMap) {
+// 	console.log(`${key} = ${value}`);
+// }
+// // 0 = zero
+// // 1 = one
 
-for (const key of myMap.keys()) {
-	console.log(key);
-}
-// 0
-// 1
+// for (const key of myMap.keys()) {
+// 	console.log(key);
+// }
+// // 0
+// // 1
 
-for (const value of myMap.values()) {
-	console.log(value);
-}
-// zero
-// one
+// for (const value of myMap.values()) {
+// 	console.log(value);
+// }
+// // zero
+// // one
 
-for (const [key, value] of myMap.entries()) {
-	console.log(`${key} = ${value}`);
-}
+// for (const [key, value] of myMap.entries()) {
+// 	console.log(`${key} = ${value}`);
+// }
+
+const kvArray = [['key1', 'value1'], ['key2', 'value2']];
+const myMap = new Map(kvArray);
+console.log(Array.from(myMap.keys()));
+console.log(Array.from(myMap.values()));
