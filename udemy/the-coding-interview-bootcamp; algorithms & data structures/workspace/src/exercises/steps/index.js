57c02e1cd18c4d40c9934ecc4c08d12dc88a893e
johnvincentio <john@johnvincent.io>
2019-06-04 15:02:27 -0400

    add

--- udemy/the-coding-interview-bootcamp; algorithms & data structures/workspace/src/exercises/steps/index.js
+++ udemy/the-coding-interview-bootcamp; algorithms & data structures/workspace/src/exercises/steps/index.js
@@ -17,6 +17,84 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+/* eslint-disable no-plusplus */
+
+function steps_1(n, row = 0, stair = '') {
+	if (row === n) {
+		return;
+	}
+	if (n === stair.length) {
+		console.log(stair);
+		steps(n, row + 1);
+		return;
+	}
+
+	if (stair.length <= row) {
+		stair += '#';
+	} else {
+		stair += ' ';
+	}
+	steps(n, row, stair);
+}
+
+function steps_3(n) {
+	for (let row = 1; row <= n; row++) {
+		let result = '';
+		for (let column = 0; column < n; column++) {
+			if (column < row) {
+				result += '#';
+			} else {
+				result += ' ';
+			}
+		}
+		console.log(result);
+	}
+}
+
+function nextChar_2(str = '', i, m, n) {
+	if (i <= n) {
+		return nextChar_2(str + (i <= m ? '#' : ' '), i + 1, m, n);
+	}
+	return str;
+}
+
+function steps_2(n) {
+	for (let m = 1; m <= n; m++) {
+		const result = nextChar_2('', 1, m, n);
+		console.log(result);
+	}
+}
+
+function makeChars(m, n) {
+	let result = '';
+	for (let i = 0; i < n; i++) {
+		if (i < m) {
+			result += '#';
+		} else {
+			result += ' ';
+		}
+	}
+	return result;
+}
+
+function steps(n) {
+	for (let m = 1; m <= n; m++) {
+		console.log(makeChars(m, n));
+	}
+}
+
+function makeChars_ok(char, n) {
+	let result = '';
+	for (let i = 0; i < n; i++) {
+		result += char;
+	}
+	return result;
+}
+
+function steps_ok(n) {
+	for (let i = 1; i <= n; i++) {
+		console.log(`${makeChars('#', i)}${makeChars(' ', n - i)}`);
+	}
+}
 
 module.exports = steps;
