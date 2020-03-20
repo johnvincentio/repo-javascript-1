0c456e979141fad03f4dba7175bbfb4ad571e6d1
johnvincentio <john@johnvincent.io>
2020-03-20 18:27:45 -0400

    add

--- linkedin/t2/jv8.js
+++ linkedin/t2/jv8.js
@@ -4,12 +4,12 @@
 */
 function deleteMessages() {
 	let container = document.querySelector(".msg-thread.relative.display-flex artdeco-dropdown artdeco-dropdown-content");
-	console.log("container ",container);
+	// console.log("container ",container);
 	let items = container.querySelectorAll("artdeco-dropdown-item");
 	
 	for (let i = 0; i < items.length; i++) {
 		let txt = items[i].textContent.trim();
-		console.log('txt ', txt);
+		// console.log('txt ', txt);
 		switch (txt) {
 			case "Delete":
 				items[i].click();		// click on delete option
@@ -23,22 +23,24 @@ function deleteMessages() {
 */
 function confirmDelete() {
 	let messageModal = document.querySelector("#artdeco-modal-outlet");
-	console.log('messageModal ', messageModal);
+	// console.log('messageModal ', messageModal);
 	if (! messageModal) return;
 
 	let allBtns = messageModal.querySelectorAll(".artdeco-modal .artdeco-modal__actionbar button");
-	console.log('allBtns ', allBtns);
+	// console.log('allBtns ', allBtns);
 	if (! allBtns) return;
 
-	let deleteBtn = allBtns[0];			// to delete, use [1]
-	console.log('deleteBtn ', deleteBtn);
+	let deleteBtn = allBtns[1];			// to delete, use [1], to not delete use [0]
+	// console.log('deleteBtn ', deleteBtn);
 	if (! deleteBtn) return;
 
 	deleteBtn.click();
 }
 
+let count = 0;
 var intervalId = setInterval(function() {
-	var delay = 500;
+	if (count > 2) return;
+	count++;
 
 	let elCard = document.querySelector(".msg-conversations-container__conversations-list .msg-conversation-listitem");
 	console.log("elCard ", elCard);
